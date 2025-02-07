67ec21f6f09fbb9861723db0e16d0efc544fcb19
shuishihan <11583895+shuishihan@users.noreply.github.com>
2025-02-07 23:48:45 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -128,12 +128,12 @@ git_sparse_clone main https://github.com/linkease/istore luci
 #修改雅典娜和亚瑟内核大小为12M
 sed -i 's/6144k/12288k/g' $GITHUB_WORKSPACE/openwrt/target/linux/qualcommax/image/ipq60xx.mk
 #测试，关闭LED
-mkdir -p files/etc/rc.d
+mkdir -p $GITHUB_WORKSPACE/openwrt/files/etc/rc.d
 cp $GITHUB_WORKSPACE/S99turnoffled $GITHUB_WORKSPACE/openwrt/files/etc/rc.d
 chmod 777 $GITHUB_WORKSPACE/openwrt/files/etc/rc.d/S99turnoffled
 #自定义wifi设置
-WIFI_SH="./package/base-files/files/etc/uci-defaults/990_set-wireless.sh"
-WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
+WIFI_SH="$GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/uci-defaults/990_set-wireless.sh"
+WIFI_UC="$GITHUB_WORKSPACE/openwrt//package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
 if [ -f "$WIFI_SH" ]; then
 	#修改WIFI名称
 	sed -i "s/BASE_SSID='.*'/BASE_SSID='Seven Win_2.4G'/g" $WIFI_SH
@@ -146,15 +146,15 @@ elif [ -f "$WIFI_UC" ]; then
 	sed -i "s/key='.*'/key='13456788'/g" $WIFI_UC
 	
 #preset openclash core
-mkdir -p files/etc/openclash/core
+mkdir -p $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep -v 'v3')
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
-wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
-wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
-chmod 755 files/etc/openclash/core/clash*
+wget -qO- $CLASH_DEV_URL | tar xOvz > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/GeoSite.dat
+chmod 755 $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash*
