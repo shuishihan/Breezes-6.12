3c9d55118dbf5d6b21f999708dfe8eebb2a8026a
shuishihan <11583895+shuishihan@users.noreply.github.com>
2025-02-07 12:54:33 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
@@ -124,3 +124,9 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+#修改雅典娜和亚瑟内核大小为12M
+sed -i 's/6144k/12288k/g' $GITHUB_WORKSPACE/wrt/target/linux/qualcommax/image/ipq60xx.mk
+#关闭LED
+mkdir -p files/etc/rc.d
+cp ../S99turnoffled $GITHUB_WORKSPACE/wrt/files/etc/rc.d
+chmod 777 $GITHUB_WORKSPACE/wrt/files/etc/rc.d/S99turnoffled
